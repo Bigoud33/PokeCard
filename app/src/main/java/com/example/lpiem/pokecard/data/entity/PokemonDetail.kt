213577e93431cb621242fb2c8d58e221e1ca84b9
Bigoud <bigoud33@gmail.com>
2019-03-20 20:22:29 +0100

    dataClass PokemonDetail

---
+++ app/src/main/java/com/example/lpiem/pokecard/data/entity/PokemonDetail.kt
@@ -0,0 +1,3 @@
+package com.example.lpiem.pokecard.data.entity
+
+data class PokemonDetail(val id: String, val name: String, val sprite: String, val hp: Int, val speed: Int, val attack: Int, val defense: Int, val specialAttack: Int, val specialDefense: Int, val price: Int)
