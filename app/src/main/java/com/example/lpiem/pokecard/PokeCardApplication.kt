192501c1cf4dbd5505ac0a740ce2796e6127d23b
Bigoud <bigoud33@gmail.com>
2019-04-02 19:42:23 +0200

    add picasso

--- app/src/main/java/com/example/lpiem/pokecard/PokeCardApplication.kt
+++ app/src/main/java/com/example/lpiem/pokecard/PokeCardApplication.kt
@@ -11,6 +11,9 @@ import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
 import javax.inject.Inject
+import com.squareup.picasso.Picasso
+
+
 
 class PokeCardApplication : Application(), HasActivityInjector {
 
@@ -19,18 +22,43 @@ class PokeCardApplication : Application(), HasActivityInjector {
 
     override fun onCreate() {
         super.onCreate()
+        initDagger()
+        initTimber()
+        initFacebook()
+        initPicasso()
+
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> = activityInjector
+
+    fun initDagger() {
         DaggerAppComponent
             .builder()
             .application(this)
             .appModule(AppModule(this))
             .build()
             .inject(this)
+    }
 
+    fun initTimber() {
         Timber.uprootAll()
         Timber.plant(Timber.DebugTree())
+    }
+
+    fun initFacebook() {
         FacebookSdk.sdkInitialize(applicationContext)
         AppEventsLogger.activateApp(this)
     }
 
-    override fun activityInjector(): AndroidInjector<Activity> = activityInjector
+    fun initPicasso() {
+        val picassoBuilder = Picasso.Builder(this)
+        val picasso = picassoBuilder.build()
+        try {
+            Picasso.setSingletonInstance(picasso)
+        } catch (ignored: IllegalStateException) {
+            // Picasso instance was already set
+            // cannot set it after Picasso.with(Context) was already in use
+        }
+
+    }
 }
