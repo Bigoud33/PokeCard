48e443083e9a945329a8e55877b7b63555f5f099
Brandon <bigoud33@gmail.com>
2019-04-10 14:05:39 +0200

    fix

--- app/src/main/java/com/example/lpiem/pokecard/data/entity/SecondResponse.kt
+++ app/src/main/java/com/example/lpiem/pokecard/data/entity/SecondResponse.kt
@@ -1,3 +1,3 @@
 package com.example.lpiem.pokecard.data.entity
 
-data class SecondResponse(val exchangeId: String,  val userId: UserId, val response : String)
+data class SecondResponse(val exchangeId: String,  val userId: String, val response : String)
