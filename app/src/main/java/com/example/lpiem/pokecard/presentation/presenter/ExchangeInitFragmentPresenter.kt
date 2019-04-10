21178439f4ef962c71fd886f8c00812bcaf068ba
Brandon <bigoud33@gmail.com>
2019-04-10 13:59:58 +0200

    createExchange

--- app/src/main/java/com/example/lpiem/pokecard/presentation/presenter/ExchangeInitFragmentPresenter.kt
+++ app/src/main/java/com/example/lpiem/pokecard/presentation/presenter/ExchangeInitFragmentPresenter.kt
@@ -2,10 +2,63 @@ package com.example.lpiem.pokecard.presentation.presenter
 
 import android.content.Context
 import com.example.lpiem.pokecard.base.BasePresenter
+import com.example.lpiem.pokecard.data.entity.ExchangeRequest
 import com.example.lpiem.pokecard.data.network.PokeCardService
+import com.example.lpiem.pokecard.presentation.ui.adapter.PokemonAdapter
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class ExchangeInitFragmentPresenter
-@Inject constructor(private val service: PokeCardService, private val context: Context): BasePresenter<ExchangeInitView>() {
+@Inject constructor(private val service: PokeCardService, private val context: Context): BasePresenter<ExchangeInitView>(), PokemonAdapter.ClickOnRecycler {
+
+    fun start(compositeDisposable: CompositeDisposable) {
+        view.displayLoader()
+        val sharedPreferences = context.getSharedPreferences("pokecard", Context.MODE_PRIVATE)
+        val userId = sharedPreferences?.getString("user-id", "null")
+        getPokemonsForUser(userId!!, compositeDisposable)
+    }
+
+    fun getPokemonsForUser(userId: String, compositeDisposable: CompositeDisposable) {
+        compositeDisposable.add(
+            service.getPokemonsForUser(userId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onError = {
+                        view.hideLoader()
+                        view.showError(it)
+                    },
+                    onSuccess = {
+                        view.hideLoader()
+                        view.showPokemons(it)
+                    }
+                )
+        )
+    }
+
+    fun initExchange(userId: String, pokemonId: String, userIdTarget: String, compositeDisposable: CompositeDisposable) {
+        compositeDisposable.add(
+            service.initExchangeRequest(ExchangeRequest(userId, pokemonId, userIdTarget))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onError = {
+                        view.hideLoader()
+                        view.showError(it)
+                    },
+                    onSuccess = {
+                        view.hideLoader()
+                        view.executeResponse(it)
+                    }
+                )
+        )
+    }
+
+    override fun itemClicked(pokemonId: String, context: Context) {
+        view.updatePokemonSelected(pokemonId)
+    }
 
 }
--- app/src/main/java/com/example/lpiem/pokecard/presentation/ui/fragments/ExchangeInitFragment.kt
+++ app/src/main/java/com/example/lpiem/pokecard/presentation/ui/fragments/ExchangeInitFragment.kt
@@ -2,10 +2,16 @@ package com.example.lpiem.pokecard.presentation.ui.fragments
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.lpiem.pokecard.R
 import com.example.lpiem.pokecard.base.BaseFragment
+import com.example.lpiem.pokecard.data.entity.ExchangeServerResponse
+import com.example.lpiem.pokecard.data.entity.Pokemons
 import com.example.lpiem.pokecard.presentation.presenter.ExchangeInitFragmentPresenter
 import com.example.lpiem.pokecard.presentation.presenter.ExchangeInitView
+import com.example.lpiem.pokecard.presentation.ui.adapter.PokemonAdapter
 import dagger.android.support.AndroidSupportInjection
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.fragment_exchange_init.*
@@ -27,25 +33,59 @@ class ExchangeInitFragment : BaseFragment<ExchangeInitFragmentPresenter>(), Exch
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         presenter.attach(this)
+        recyclerViewPokedex.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        presenter.start(compositeDisposable)
         val sharedPreferences = context!!.getSharedPreferences("pokecard", Context.MODE_PRIVATE)
         val userIdStr = sharedPreferences?.getString("user-id", "null")
         exchangeInitButton.setOnClickListener {
             if (userIdTextEdit.text!!.isNotBlank() && pokemonId != null) {
-
+                presenter.initExchange(userIdStr!!, pokemonId!!, userIdTextEdit.text.toString(), compositeDisposable)
+            } else {
+                Toast.makeText(context, "Veillez remplir tous les champs", Toast.LENGTH_LONG).show()
             }
         }
     }
 
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
+
+    override fun showPokemons(pokemonsList: Pokemons) {
+        val adapter = PokemonAdapter(pokemonsList, presenter)
+        recyclerViewPokedex.adapter = adapter
+    }
+
+    override fun updatePokemonSelected(pokemonId: String) {
+        this.pokemonId = pokemonId
+        pokemonIdTextView.text = pokemonId
+    }
+
+    override fun executeResponse(exchangeServerResponse: ExchangeServerResponse) {
+        if (exchangeServerResponse.error.isNullOrBlank()) {
+            showExchangeFragment()
+        } else {
+            Toast.makeText(context, exchangeServerResponse.error, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun showExchangeFragment() {
+        activity!!.supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.activityContainer, ExchangeFragment())
+            .commit()
+    }
+
     override fun displayLoader() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        loader.show()
     }
 
     override fun hideLoader() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        loader.hide()
     }
 
-    override fun showError(errorMessage: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun showError(errorMessage: Throwable) {
+        Toast.makeText(context, errorMessage.localizedMessage, Toast.LENGTH_LONG).show()
     }
 
 }
